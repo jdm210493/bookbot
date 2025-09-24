54c89322f86b58bcb0a9ab4663420aa202f575a5
jdm210493 <jermaine_de_mees@hotmail.com>
2025-09-24 18:24:16 +0200

    added sorted_list function to stats and updated main.py accordingly

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 
 # python
-from stats import count_words, count_chars
+from stats import count_words, count_chars, sorted_list
 
-def get_book_text(filepath):
-    #open the file at 'filepath' and ensure it closes automatically
-     with open(filepath) as f:
-        # read the entire file into a single string
-        book_content = f.read()
-        # give the text back to the caller
+def get_book_text(filepath): # open the file at 'filepath' and ensure it closes automatically
+     with open(filepath) as f: # read the entire file into a single string
+        book_content = f.read() # give the text back to the caller
         return book_content
 
 def main():
-    # relative path to the book file from this script
-    path = "books/frankenstein.txt"
-    # get the book text by reading the file
-    book_content = get_book_text(path)
+    path = "books/frankenstein.txt" # relative path to the book file from this script
+    book_content = get_book_text(path) # get the book text by reading the file
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
 
     num_words = count_words(book_content)
-    print(f"{num_words} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
 
-    chars = count_chars(book_content)
-    print(chars)
+    chars = count_chars(book_content) # Calls the count_chars function to count how many times each character appears in book_content.
+    sorted_chars = sorted_list(chars) # Passes the character count dictionary to sorted_list, which returns a sorted list of dictionaries
+
+    print("--------- Character Count -------")
+    for item in sorted_chars: #  loops through the sorted list and prints each character and its count in the format: character: count
+        print (f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 # run the program when this file is executed
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,15 @@ def count_chars(text):
         else:
             counts[ch] = 1
     return counts
+
+def sorted_list(counts): # this function takes a dictionary called counts 
+    keys_list = [] # it creates an empty list
+    for k, v in counts.items(): # it loops through eack key-value pair
+        if k.isalpha(): # if the key is an alphabetical character, it adds a dictionary to keys_list with the character and its count
+            keys_list.append({"char": k, "num": v})
+
+    def sort_on(item): # it defines a helper function sort_on that returns the "num" value from each dictionary
+        return item["num"]
+
+    keys_list.sort(reverse=True, key=sort_on) # it sorts keys_list in descending order by count using the helper function
+    return keys_list # it returns the sorted list
