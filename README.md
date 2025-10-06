eefbe90120bb1c210468a8a72205525df08ac30f
jdm210493 <jermaine_de_mees@hotmail.com>
2025-10-06 13:33:20 +0200

    added command-line arguments for flexibility via sys.argv

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # bookbot
 
 BookBot is my first [Boot.dev](https://www.boot.dev) project!# bookbot
+
+This script reads a book file, counts words and characters, sorts the character counts, and prints a report to the console. It uses command-line arguments for flexibility.
--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-
 # python
+import sys
 from stats import count_words, count_chars, sorted_list
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    path = sys.argv[1]
 
 def get_book_text(filepath): # open the file at 'filepath' and ensure it closes automatically
      with open(filepath) as f: # read the entire file into a single string
-        book_content = f.read() # give the text back to the caller
-        return book_content
+        return f.read()
 
-def main():
-    path = "books/frankenstein.txt" # relative path to the book file from this script
-    book_content = get_book_text(path) # get the book text by reading the file
+def main(path):
+    text = get_book_text(path) # get the book text by reading the file
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}...")
 
-    num_words = count_words(book_content)
+    num_words = count_words(text)
+
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
 
-    chars = count_chars(book_content) # Calls the count_chars function to count how many times each character appears in book_content.
+    chars = count_chars(text) # Calls the count_chars function to count how many times each character appears in book_content.
     sorted_chars = sorted_list(chars) # Passes the character count dictionary to sorted_list, which returns a sorted list of dictionaries
 
     print("--------- Character Count -------")
+ 
     for item in sorted_chars: #  loops through the sorted list and prints each character and its count in the format: character: count
-        print (f"{item['char']}: {item['num']}")
+       print (f"{item['char']}: {item['num']}")
 
     print("============= END ===============")
 
-# run the program when this file is executed
-main()
+if __name__ == "__main__":
+    main(path)
